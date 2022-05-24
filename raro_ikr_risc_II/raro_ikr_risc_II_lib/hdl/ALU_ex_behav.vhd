a55954e0f8f3bb17ecf1cb9d7d950c72fd293214
Louis Petrick <st179909@stud.uni-stuttgart.de>
2022-05-24 16:29:30 +0200

    prepared alu for copy paste

--- raro_ikr_risc_II/raro_ikr_risc_II_lib/hdl/ALU_ex_behav.vhd
+++ raro_ikr_risc_II/raro_ikr_risc_II_lib/hdl/ALU_ex_behav.vhd
@@ -11,8 +11,26 @@ ARCHITECTURE behav OF ALU_ex IS
 
 
 BEGIN
+  alu: process is
+    variable x, y: word;
+    variable c_in: std_logic;
+    variable c_word: word;
+    
+    variable au_x, au_y: word;
+    variable au_l, au_f: word;
+    variable au_h: std_logic_vector(word'left + 1 downto word'left);
+    variable au_c, au_v: std_logic;
+    
+    variable f: word;
+  
+  begin
 
-
+    
+    
+  end process alu;
+  
+  
+  
 
 END ARCHITECTURE behav;
 
